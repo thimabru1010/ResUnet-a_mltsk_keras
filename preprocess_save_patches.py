25c715037e0ecbb2d96c9650ed0a4f10716beddc
Thiago Matheus <thiago.matheus@cyberlabs.com.br>
2020-07-20 22:00:49 -0300

    Prints adicionados

--- preprocess_save_patches.py
+++ preprocess_save_patches.py
@@ -198,6 +198,7 @@ stride = patch_size // 8
 
 #stride = patch_size
 patches = extract_patches_hw(img_train_normalized, binary_img_train_ref, patch_size, stride)
+print('patches extraidos!')
 # patches_tr, patches_tr_ref = bal_aug_patches2(percent, patch_size, patches_tr, patches_tr_ref)
 patches_tr = patches[0].tolist()
 patches_tr_ref = patches[1].tolist()
