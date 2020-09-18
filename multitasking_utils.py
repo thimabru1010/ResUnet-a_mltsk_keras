c02fc51669ba628d64aac91086356326b7e888bd
Thiago Matheus <thiago.matheus@cyberlabs.com.br>
2020-09-17 23:54:03 -0300

    Cleaning multitasking utils

--- multitasking_utils.py
+++ multitasking_utils.py
@@ -2,30 +2,6 @@ import tensorflow as tf
 import cv2
 import numpy as np
 
-# def get_boundary_labels(labels, _kernel_size = (3,3)):
-#
-#     labels = labels.copy()
-#     num_patches, h, w, c = labels.shape
-#     bounds = np.empty_like(labels,dtype=np.float32)
-#     for n in range(num_patches):
-#         label = labels[n,:,:,:]
-#         for channel in range(c):
-#             #print(label)
-#             label = label.astype(np.uint8)
-#             #print(label)
-#             #temp = cv2.Canny(label[:,:,channel],0,1)
-#             bound = cv2.Canny(label[:,:,channel],0,1)
-#             bound = cv2.dilate(bound, cv2.getStructuringElement(cv2.MORPH_CROSS,_kernel_size) ,iterations = 1)
-#
-#             # bound = bound.astype(np.float32)
-#             # bounds /= 255.
-#
-#             bounds[n,:,:,channel] = bound
-#
-#     bounds = bounds.astype(np.float32)
-#     bounds /= 255.
-#     return bounds
-
 def get_boundary_label(label, kernel_size=(3, 3)):
     _, _, channel = label.shape
     bounds = np.empty_like(label, dtype=np.float32)
@@ -45,23 +21,6 @@ def get_boundary_label(label, kernel_size=(3, 3)):
         bounds[:, :, c] = tlabel
     return bounds
 
-# def get_distance_labels(labels):
-#     labels = labels.copy()
-#     #print (label.shape)
-#     dists = np.empty_like(labels,dtype=np.float32)
-#     num_patches, h, w, c = labels.shape
-#     # print('='*10 + ' Distance ' + '='*10)
-#     # print(labels.shape)
-#     for n in range(num_patches):
-#         label = labels[n,:,:,:]
-#         for channel in range(c):
-#             label = label.astype(np.uint8)
-#             dist = cv2.distanceTransform(label[:,:,channel], cv2.DIST_L2, 0)
-#             dist = cv2.normalize(dist, dist, 0, 1.0, cv2.NORM_MINMAX)
-#             dists[n,:,:,channel] = dist
-#
-#     return dists
-
 
 def get_distance_label(label):
     label = label.copy()
@@ -73,22 +32,6 @@ def get_distance_label(label):
         dists[:, :, channel] = dist
 
     return dists
-
-
-# def Tanimoto_loss(label,pred):
-#     square_pred=tf.square(pred)
-#     square_label=tf.square(label)
-#     add_squared_label_pred = tf.add(square_pred,square_label)
-#     # Ver isso aqui
-#     sum_square=tf.reduce_sum(add_squared_label_pred,axis=-1)
-#
-#     product=tf.multiply(pred,label)
-#     sum_product=tf.reduce_sum(product,axis=-1)
-#
-#     denomintor=tf.subtract(sum_square,sum_product)
-#     loss=tf.divide(sum_product,denomintor)
-#     loss=tf.reduce_mean(loss)
-#     return 1.0-loss
 
 
 def Tanimoto_loss(label,pred):
