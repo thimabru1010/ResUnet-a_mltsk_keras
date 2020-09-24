add2b0c47dc5ee70b34c26e448a5754cf6245792
Thiago Matheus <thiago.matheus@cyberlabs.com.br>
2020-09-24 15:37:00 -0300

    Testing custom metric

--- train_ISPRS.py
+++ train_ISPRS.py
@@ -95,6 +95,21 @@ def compute_metrics_hw(true_labels, predicted_labels):
     precision = 100*precision_score(true_labels, predicted_labels, average=None)
     return accuracy, f1score, recall, precision
 
+def compute_metrics_seg(y_true, y_pred):
+    print('[CHECKING METRICS]')
+    print('y_true')
+    print(y_true.shape)
+    print(y_true)
+    print('y_pred')
+    print(y_pred.shape)
+    print(y_pred)
+    accuracy = 100*accuracy_score(y_true, y_pred)
+    #avg_accuracy = 100*accuracy_score(y_true, y_pred, average=None)
+    f1score = 100*f1_score(y_true, y_pred, average=None)
+    recall = 100*recall_score(y_true, y_pred, average=None)
+    precision = 100*precision_score(y_true, y_pred, average=None)
+    return accuracy, f1score, recall, precision
+
 
 def add_tensorboard_scalars(train_writer, val_writer, epoch,
                             metric_name, train_loss, val_loss,
@@ -214,12 +229,12 @@ def train_model(args, net, x_train_paths, y_train_paths, x_val_paths,
 
                 loss_tr = loss_tr + net.train_on_batch(x=x_train_b, y=y_train_b)
 
-            # print('='*30 + ' [CHECKING LOSS] ' + '='*30)
-            # print(net.metrics_names)
-            # print(type(loss_tr))
-            # print(len(loss_tr))
-            # print(loss_tr)
-            # print(loss_tr.shape)
+            print('='*30 + ' [CHECKING LOSS] ' + '='*30)
+            print(net.metrics_names)
+            print(type(loss_tr))
+            print(len(loss_tr))
+            print(loss_tr)
+            print(loss_tr.shape)
 
         # Training loss; Divide by the number of batches
         print(loss_tr)
@@ -556,7 +571,7 @@ if __name__ == '__main__':
             resuneta = Resunet_a((rows, cols, channels), args.num_classes, args)
             model = resuneta.model
             model.summary()
-            model.compile(optimizer=optm, loss=loss, metrics=['accuracy'])
+            model.compile(optimizer=optm, loss=loss, metrics=['accuracy', compute_metrics_seg])
 
         print('ResUnet-a compiled!')
     else:
@@ -592,104 +607,3 @@ if __name__ == '__main__':
 
         end_time = time.time() - start_time
         print(f'\nTraining took: {end_time / 3600} \n')
-
-    # #%% Test model
-    #
-    # # Load images
-    # img_test_path = 'Image_Test.npy'
-    # img_test = load_npy_image(os.path.join(root_path, img_test_path)).astype(np.float32)
-    # # Normalizes the image
-    # img_test_normalized = normalization(img_test)
-    # # Transform the image into W x H x C shape
-    # img_test_normalized = img_test_normalized.transpose((1,2,0))
-    # print(img_test_normalized.shape)
-    #
-    # # Load reference
-    # img_test_ref_path = 'Reference_Test.npy'
-    # img_test_ref = load_npy_image(os.path.join(root_path, img_test_ref_path))
-    # img_test_ref = img_test_ref.transpose((1,2,0))
-    # print(img_test_ref.shape)
-    #
-    # # Create binarized matrix
-    # w = img_test_ref.shape[0]
-    # h = img_test_ref.shape[1]
-    # c = img_test_ref.shape[2]
-    # # binary_img_train_ref = np.zeros((1,w,h))
-    # binary_img_test_ref = np.full((w, h), -1)
-    # # Dictionary used in training
-    # label_dict = {'(255, 255, 255)': 0, '(0, 255, 0)': 1, '(0, 255, 255)': 2, '(0, 0, 255)': 3, '(255, 255, 0)': 4}
-    # # label = 0
-    # for i in range(w):
-    #     for j in range(h):
-    #         r = img_test_ref[i][j][0]
-    #         g = img_test_ref[i][j][1]
-    #         b = img_test_ref[i][j][2]
-    #         rgb = (r,g,b)
-    #         rgb_key = str(rgb)
-    #         binary_img_test_ref[i][j] = label_dict[rgb_key]
-    # print(label_dict)
-    #
-    # # Put the patch size according to you training here
-    # patches_test = extract_patches_train(img_test_normalized, patch_size)
-    # patches_test_ref = extract_patches_test(binary_img_test_ref, patch_size)
-    #
-    # #% Load model
-    # model = load_model(filepath+'unet_exp_'+str(exp)+'.h5', compile=False)
-    # # Prediction
-    # # Test the model
-    # patches_pred = Test(model, patches_test, args)
-    # print(patches_pred.shape)
-    #
-    # # Metrics
-    # true_labels = np.reshape(patches_test_ref, (patches_test_ref.shape[0]* patches_test_ref.shape[1]*patches_test_ref.shape[2]))
-    # predicted_labels = np.reshape(patches_pred, (patches_pred.shape[0]* patches_pred.shape[1]*patches_pred.shape[2]))
-    #
-    # # Metrics
-    # metrics = compute_metrics(true_labels,predicted_labels)
-    # cm = confusion_matrix(true_labels, predicted_labels, labels=[0,1,2,3,4])
-    #
-    # print('Confusion  matrix \n', cm)
-    # print()
-    # print('Accuracy: ', metrics[0])
-    # print('F1score: ', metrics[1])
-    # print('Recall: ', metrics[2])
-    # print('Precision: ', metrics[3])
-    #
-    # def pred_recostruction(patch_size, pred_labels, binary_img_test_ref):
-    #     # Patches Reconstruction
-    #     stride = patch_size
-    #
-    #     height, width = binary_img_test_ref.shape
-    #
-    #     num_patches_h = height // stride
-    #     num_patches_w = width // stride
-    #
-    #     new_shape = (height, width)
-    #     img_reconstructed = np.zeros(new_shape)
-    #     cont = 0
-    #     # rows
-    #     for h in range(num_patches_h):
-    #         # columns
-    #         for w in range(num_patches_w):
-    #             img_reconstructed[h*stride:(h+1)*stride, w*stride:(w+1)*stride] = patches_pred[cont]
-    #             cont += 1
-    #     print('Reconstruction Done!')
-    #     return img_reconstructed
-    #
-    #
-    # def reconstruction_rgb_prdiction_patches(img_reconstructed, label_dict):
-    #     reversed_label_dict = {value : key for (key, value) in label_dict.items()}
-    #     print(reversed_label_dict)
-    #     height, width = img_reconstructed.shape
-    #     img_reconstructed_rgb = np.zeros((height,width,3))
-    #     for h in range(height):
-    #         for w in range(width):
-    #             pixel_class = img_reconstructed[h, w]
-    #             img_reconstructed_rgb[h, w, :] = ast.literal_eval(reversed_label_dict[pixel_class])
-    #     print('Conversion to RGB Done!')
-    #     return img_reconstructed_rgb.astype(np.uint8)
-    #
-    # img_reconstructed = pred_recostruction(patch_size, patches_pred, binary_img_test_ref)
-    # img_reconstructed_rgb = reconstruction_rgb_prdiction_patches(img_reconstructed, label_dict)
-    #
-    # plt.imsave(f'img_reconstructed_rgb_exp{exp}.jpeg', img_reconstructed_rgb)
